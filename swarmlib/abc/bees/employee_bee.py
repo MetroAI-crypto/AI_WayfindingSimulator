76a7a286e5be11fc7f9fde231f8021ea91cb84fa
Ivan Tomashevskiy <tomashevskiy_vanya@mail.ru>
2025-01-05 19:16:17 +0300

    fix: version_2

--- swarmlib/abc/bees/employee_bee.py
+++ swarmlib/abc/bees/employee_bee.py
@@ -1,7 +1,4 @@
-# ------------------------------------------------------------------------------------------------------
-#  Copyright (c) Leo Hanisch. All rights reserved.
-#  Licensed under the BSD 3-Clause License. See LICENSE.txt in the project root for license information.
-# ------------------------------------------------------------------------------------------------------
+
 
 import numpy as np
 from .bee_base import BeeBase
--- swarmlib/abc/bees/onlooker_bee.py
+++ swarmlib/abc/bees/onlooker_bee.py
@@ -1,7 +1,4 @@
-# ------------------------------------------------------------------------------------------------------
-#  Copyright (c) Leo Hanisch. All rights reserved.
-#  Licensed under the BSD 3-Clause License. See LICENSE.txt in the project root for license information.
-# ------------------------------------------------------------------------------------------------------
+
 
 from typing import Tuple
 from .bee_base import BeeBase
