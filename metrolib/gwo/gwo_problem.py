c0f954c4285a7c566aad977aa9bfb67b18ba2e4a
Ivan Tomashevskiy <tomashevskiy_vanya@mail.ru>
2025-01-06 18:35:04 +0300

    gwo log add

---
+++ metrolib/gwo/gwo_problem.py
@@ -0,0 +1,11 @@
+
+import logging
+from copy import deepcopy
+import numpy as np
+from .wolf import Wolf
+from .visualizer import Visualizer
+from ..util.problem_base import ProblemBase
+
+# pylint: disable=too-many-instance-attributes
+
+LOGGER = logging.getLogger(__name__)
