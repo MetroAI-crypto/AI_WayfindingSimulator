68cc5e576c52866d933cabec30cdbe842fe3fc0d
Ivan Tomashevskiy <tomashevskiy_vanya@mail.ru>
2025-01-07 17:49:27 +0300

    //

---
+++ metrolib/util/visualizer_base.py
@@ -0,0 +1,11 @@
+
+from abc import ABC, abstractmethod
+
+class VisualizerBase(ABC):
+    @abstractmethod
+    def add_data(self) -> None:
+        pass
+
+    @abstractmethod
+    def replay(self, **kwargs)-> None:
+        pass
