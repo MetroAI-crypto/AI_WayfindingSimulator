eaddc189d1680b56bfa3fa2ac6389ca2843dd1d6
Ivan Tomashevskiy <tomashevskiy_vanya@mail.ru>
2025-01-08 13:31:18 +0300

    woa main +

---
+++ metrolib/woa/main.py
@@ -0,0 +1,47 @@
+
+import logging
+
+from .woa_problem import WOAProblem
+from ..util.functions import FUNCTIONS
+
+LOGGER = logging.getLogger(__name__)
+
+
+def _run_woa(args):
+    LOGGER.debug('Start the whale optimization algorithm with parameters="%s"', args)
+    args['function'] = FUNCTIONS[args['function']]
+
+    problem = WOAProblem(**args)
+    problem.solve()
+    problem.replay()
+
+
+def configure_parser(sub_parsers):
+    """
+    Get the argument parser for the whale optimization algorithm.
+    """
+
+    parser = sub_parsers.add_parser(
+        'whales',
+        description='Solving an minimization problem using the whale optimization algorithm',
+        help='Whale optimization algorithm for minimization problem')
+
+    parser.add_argument(
+        '-u',
+        '--upper-boundary',
+        type=float,
+        default=4.,
+        help='Upper boundary of the function (default 4)')
+    parser.add_argument(
+        '-l',
+        '--lower-boundary',
+        type=float,
+        default=0.,
+        help='Lower boundary of the function (default 0)')
+   
+    parser.add_argument(
+        'whales',
+        type=int,
+        help='Number of whales used for solving')
+
+    parser.set_defaults(func=_run_woa)
